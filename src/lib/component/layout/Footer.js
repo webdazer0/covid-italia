fcdee55127828960be3755785bb6e5f4a963c2b3
MigueL C <webdazer0@gmail.com>
2023-11-25 11:34:26 +0100

    chore: add `Layout components`: Layout, Header & Footer

---
+++ src/lib/component/layout/Footer.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="container pt-5 pb-3">
+        <div className="row">
+          <div className="col-md-12 text-center">
+            <p>
+              2020 © Webdazer0 | Created with{" "}
+              <span role="img" aria-label="heart">
+                ❤🧡
+              </span>
+              <span role="img" aria-label="fire">
+                ☄
+              </span>
+              <span role="img" aria-label="star">
+                ⭐
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/lib/component/layout/Header.js
@@ -0,0 +1,56 @@
+import React from "react";
+import "./header.css";
+
+function Header() {
+  return (
+    <header>
+      <nav className="navbar navbar-expand-lg">
+        <div className="container">
+          <a className="navbar-brand" href="/">
+            Covid App
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav d-flex justify-content-between ml-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="/">
+                  {/* By region  */}
+                  <span className="sr-only">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  {/* By # deaths */}
+                </a>
+              </li>
+              <li className="nav-item d-flex align-items-center">
+                {/* otherss  */}
+              </li>
+            </ul>
+          </div>
+          <a
+            className="nav-link btn-support"
+            href="https://paypal.me/miguelcruzad0"
+          >
+            Offrimi una{" "}
+            <span className="birra" role="img" aria-label="birra">
+              🍺
+            </span>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
