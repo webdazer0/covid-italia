337da94a213260193990b39ee3431afe05420f0a
MigueL C <webdazer0@gmail.com>
2023-12-10 13:04:12 +0100

    chore: migrate react from `CRA` to `Vite`

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ import { startWelcome } from "./utils/logger";
 
 startWelcome();
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
    <React.StrictMode>
     <Provider store={store}>
       <App />
